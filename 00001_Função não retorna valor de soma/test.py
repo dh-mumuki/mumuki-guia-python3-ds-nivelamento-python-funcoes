c451224fc3f946cfd8fc1c57f2cb97b428227223
vmatos@digitalhouse.com <vmatos@digitalhouse.com>
2021-03-31 23:15:11 +0000

    Mumuki Export on 2021-03-31 23:15:11 +0000

--- 00001_Função não retorna valor de soma/test.py
+++ 00001_Função não retorna valor de soma/test.py
@@ -1,4 +1,24 @@
+import random
+
 class Test(unittest.TestCase):
 
   def positivos(self):
+    # Soma de positivos
     self.assertAlmostEqual(soma(2, 3), 5)
+    self.assertAlmostEqual(soma(1, 2), 3)
+    self.assertAlmostEqual(soma(5, 6), 11)
+    self.assertAlmostEqual(soma(9, 9), 18)
+    
+    # Soma de negativos e positivos
+    self.assertAlmostEqual(soma(-2, 3), 1)
+    self.assertAlmostEqual(soma(1, -2), -1)
+    self.assertAlmostEqual(soma(5, -6), -1)
+    self.assertAlmostEqual(soma(9, -9), 0)
+    
+    # Testes aleatórios
+    a = random.randint(-100, 100)
+    b = random.randint(-100, 100)
+    self.assertAlmostEqual(soma(a, b), a + b)
+    self.assertAlmostEqual(soma(a, b), a + b)
+    self.assertAlmostEqual(soma(a, b), a + b)
+    self.assertAlmostEqual(soma(a, b), a + b)
